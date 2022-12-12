b246dcca14529a9bb9df429ecde42157fbfce126
Tuhin Biswas <biswastuhin1074@gmail.com>
2022-12-13 01:36:01 +0530

    hostting...

--- mcoder/mcoder/urls.py
+++ mcoder/mcoder/urls.py
@@ -18,6 +18,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.conf.urls import url
+
 admin.site.site_header = 'Mcoder Admin'
 admin.site.site_title = 'Mcoder Admin Panel'
 admin.site.index_title = 'Welcome to Mcoder Admin Panel'
@@ -26,4 +29,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('blog/', include('blog.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
