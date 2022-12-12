78dbc3b91946f9260b61b5a98c8bd64952beba8c
Tuhin Biswas <biswastuhin1074@gmail.com>
2022-12-13 03:41:29 +0530

    set

--- mcoder/mcoder/settings.py
+++ mcoder/mcoder/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-mi7rhlxz7d4w7!f53&ca$&x^n47v%+ep4xd_chu$qyq#+aq944'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
@@ -138,9 +138,9 @@ STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
-EMAIL_BACKEND = config('EMAIL_BACKEND')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_BACKEND = django.core.mail.backends.smtp.EmailBackend
+EMAIL_HOST = smtp.gmail.com
+EMAIL_PORT = 587
+EMAIL_HOST_USER = biswastuhin1074@gmail.com
+EMAIL_HOST_PASSWORD = cmoywwqxzwaelgek
+EMAIL_USE_TLS = True
