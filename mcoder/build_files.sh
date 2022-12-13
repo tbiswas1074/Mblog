1d735a1e805a123ef6c40a5b30ca88ef90b66328
Tuhin Biswas <biswastuhin1074@gmail.com>
2022-12-13 18:44:19 +0530

    gitignore...

--- mcoder/build_files.sh
+++ mcoder/build_files.sh
@@ -1,8 +1,12 @@
 echo " BUILD START"
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo " BUILD END" 
 
+echo "Make Migration..." 
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.9 manage.py collectstatic --noinput --clear
 
 
 
