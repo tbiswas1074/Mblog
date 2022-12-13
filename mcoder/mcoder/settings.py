e1588e1dfb1ae303fc7eca64ae28657c7e7efa78
Tuhin Biswas <biswastuhin1074@gmail.com>
2022-12-14 02:33:43 +0530

    final last

--- mcoder/mcoder/settings.py
+++ mcoder/mcoder/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = []
 
 # Application definition
 
