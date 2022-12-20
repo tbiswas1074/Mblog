64b38bda79d20e3c803aa124ddb6fbf81b178f37
Tuhin Biswas <biswastuhin1074@gmail.com>
2022-12-20 23:08:12 +0530

    final cmt

--- mcoder/mcoder/settings.py
+++ mcoder/mcoder/settings.py
@@ -79,13 +79,23 @@ WSGI_APPLICATION = 'mcoder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
