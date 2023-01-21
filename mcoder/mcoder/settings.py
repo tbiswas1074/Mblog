8f432597ce2817cc1e54e065ccb279461d86e7ae
Tuhin Biswas <biswastuhin1074@gmail.com>
2023-01-21 17:47:09 +0530

    settings

--- mcoder/mcoder/settings.py
+++ mcoder/mcoder/settings.py
@@ -144,7 +144,8 @@ STATIC_URL= "/static/"
 # MEDIA_ROOT= os.path.join(BASE_DIR, "media")
 # MEDIA_URL="/media/"
 STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = '/root/mcoder'
 
 
 
