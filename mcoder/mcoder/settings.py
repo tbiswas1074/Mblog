856a77831bd8ef23837c432a01a46f2b420bd5e7
Tuhin Biswas <biswastuhin1074@gmail.com>
2023-01-25 02:28:45 +0530

    sdf

--- mcoder/mcoder/settings.py
+++ mcoder/mcoder/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from django.contrib.messages import constants as messages
 import os
-from decouple import config
+# from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
