32a9d052bbef81e0d5d655897b5a95590cf08d2d
Tuhin Biswas <biswastuhin1074@gmail.com>
2023-01-25 23:57:40 +0530

    database

--- mcoder/mcoder/settings.py
+++ mcoder/mcoder/settings.py
@@ -82,23 +82,23 @@ WSGI_APPLICATION = 'mcoder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'mblog',
-#         'HOST': 'mblog.cxpomrecsrby.ap-south-1.rds.amazonaws.com',
-#         'USER': 'admin',
-#         'PASSWORD': 'Stu1997hin*',
-#         'PORT': '3306'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mblog',
+        'HOST': 'mblog.cxpomrecsrby.ap-south-1.rds.amazonaws.com',
+        'USER': 'admin',
+        'PASSWORD': 'Stu1997hin*',
+        'PORT': '3306'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
